583babe3f0750a3de2c9a10731c50ce7e849efcc
dianalemen <diana.lemen@gmail.com>
2021-12-16 18:10:39 +0200

    fixed path

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const waitForWriting = require('../index')
-const resultUrl = require('../../../../result.json')
+const resultUrl = require('../../../../results.json')
 const codeOwnerPath = require('../../../../.github/CODEOWNERS')
 
 waitForWriting(resultUrl, codeOwnerPath).then(() => console.log('done'))
