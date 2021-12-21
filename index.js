442c42a1d397b7289e82e824ccec0a781fa1f684
dlemen4 <dlemen@assurance.com>
2021-12-21 11:34:29 +0200

    fixed

--- index.js
+++ index.js
@@ -104,6 +104,7 @@ module.exports = () => {
   runLinterScript
     .then(() => {
       console.log("Linter finished! Ouput is creating for you...");
+      waitForWriting();
     })
     .catch(err => console.log(err))
     .finally(() => waitForWriting());
